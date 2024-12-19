f41ccf9ef15ae8883ccab90a2cd6d63214bc53b1
William Zhang <william.zhang.ml@gmail.com>
2024-12-18 22:11:00 -0500

    add rudimentary poke counter app

---
+++ poke-counter/main.py
@@ -0,0 +1,32 @@
+"""
+Poke counter application.
+This app shows the number of times the user has poked it.
+"""
+from kivy.app import App
+from kivy.properties import NumericProperty, ObjectProperty
+from kivy.uix.widget import Widget
+
+
+class Counter(Widget):
+    """Counter/label that increments when touched. """
+    label = ObjectProperty(None)
+    count = NumericProperty(0)
+
+    def __init__(self, **kwargs) -> None:
+        super().__init__(**kwargs)
+        self.label.text = str(self.count)
+
+    def on_touch_down(self, touch) -> None:
+        """Change color to red when pressed. """
+        self.count += 1
+        self.label.text = str(self.count)
+
+
+class MainApp(App):
+    """Main application class. """
+    def build(self) -> None:
+        return Counter()
+
+
+if __name__ == '__main__':
+    MainApp().run()
